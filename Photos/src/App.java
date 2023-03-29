5858d5c50d60a05979c4b54ce9cf08fb87758700
RoohitD <66574477+RoohitD@users.noreply.github.com>
2023-03-29 18:03:04 -0400

    Testing Gitignore

--- Photos/src/App.java
+++ Photos/src/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
  
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello World! I am an App.");
             }
         });
         
