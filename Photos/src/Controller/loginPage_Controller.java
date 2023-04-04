b9ac7b18cf3c4f9c58557928e1b9118a88647f9a
RoohitD <66574477+RoohitD@users.noreply.github.com>
2023-04-04 13:53:20 -0400

    ...

--- Photos/src/Controller/loginPage_Controller.java
+++ Photos/src/Controller/loginPage_Controller.java
@@ -1,5 +1,14 @@
 package Controller;
 
-public class loginPage_Controller {
-    
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class loginPage_Controller implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
 }
--- Photos/src/Controller/mainApp.java
+++ Photos/src/Controller/mainApp.java
@@ -2,26 +2,31 @@ package Controller;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class mainApp extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) throws IOException {
+
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/Users/rohitdeshmukh/Desktop/Coding Projects/Photos_App/Photos/src/scene/Login-page.fxml"));
+        loader.setLocation(getClass().getResource("/src/Controller/Login-page.fxml"));
 
         AnchorPane root = (AnchorPane)loader.load();
-        
-        
-        
-        primaryStage.setTitle("Login Page");
+
+        primaryStage.setTitle("Song Library");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
